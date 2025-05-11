d9331f79f465cfe9da3ec1824638b33adae5ce8a
liston9 <142957646+liston9@users.noreply.github.com>
2025-05-10 23:18:49 -0600

    finished basic file compressions locally
    sending post request with input and output filenames in JSON to localhost is handled by express to use ffmpeg (installed locally) to compress the video to 360p

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,36 @@
-import express from 'express';
+import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
 
+app.post('/process-video', (req, res) => {
+  res.send('POST request to homepage')
+  const inputVideoPath = req.body.inputVideoPath; // Path to the input video file
+  const outputVideoPath = req.body.outputVideoPath; // Path to save the processed video
+
+  if (!inputVideoPath) {
+    res.status(400).send("Input video path is required");
+  }
+  if (!outputVideoPath) {
+    res.status(400).send("Output video path is required");
+  }
+  ffmpeg(inputVideoPath)
+    .outputOptions("-vf", "scale=-1:360")
+    .on("end", function() {
+      console.log("Video processing finished successfully");
+      res.status(200).send("Video processing finished successfully");
+    })
+    .on("error", (err) => {
+      console.log("Error processing video: "+err.message);
+      res.status(500).send("Internal server error: " + err.message);
+    })
+    .save(outputVideoPath);
+})
+
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
